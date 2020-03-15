effb0a58e579606f5008da1e325ee3501f67ee6d
Bill Hass <billhass@umich.edu>
2020-03-15 00:24:18 -0400

    Apply patch: flex: use 2.6.3 by default Fixes segfault in stage1scan.c when building https://github.com/spack/spack/commit/d579231967d502b56b6254c660e6dfddc4f2ba3a

--- package/flex/flex.mk
+++ package/flex/flex.mk
@@ -4,14 +4,15 @@
 #
 ################################################################################
 
-FLEX_VERSION = 2.6.4
-FLEX_SITE = https://github.com/westes/flex/files/981163
+FLEX_VERSION = 2.6.3
+FLEX_SITE = https://github.com/westes/flex/releases/download/v2.6.3/flex-2.6.3.tar.gz 
 FLEX_INSTALL_STAGING = YES
 FLEX_LICENSE = FLEX
 FLEX_LICENSE_FILES = COPYING
 FLEX_DEPENDENCIES = $(TARGET_NLS_DEPENDENCIES) host-m4
 FLEX_CONF_ENV = ac_cv_path_M4=/usr/bin/m4
 HOST_FLEX_DEPENDENCIES = host-m4
+FLEX_CFLAGS = -D_GNU_SOURCE
 
 define FLEX_DISABLE_PROGRAM
 	$(SED) 's/^bin_PROGRAMS.*//' $(@D)/src/Makefile.in
